846ff11a5cf61471a4d6842e11b47042b7a54025
mariyaongit <mariyafrancis96@gmail.com>
2024-07-18 19:23:18 -0400

    add comments

--- main_test.go
+++ main_test.go
@@ -7,12 +7,12 @@ func TestCountVowels(t *testing.T) {
 		userstring    string
 		expected int
 	}{
-		{"vowelcount", 4},         
-        {"aeiou", 5},              
-        {"AEIOU", 5},              
-        {"", 0},                   
-        {"#a@e$i", 3},             
-        {"Hello, How are you", 7},  
+        {"vowelcount", 4},         // Test case with mixed letters
+        {"aeiou", 5},              // Test case with all vowels
+        {"AEIOU", 5},              // Test case with uppercase vowels
+        {"", 0},                   // Test case with an empty string
+        {"#a@e$i", 3},             // Test case with special characters and vowels
+        {"Hello, How are you", 7}, // Test case with a sentence
 	}
 
 	for _, tc := range testCases {
